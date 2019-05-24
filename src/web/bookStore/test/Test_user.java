63348da52287dcbe9fefa197cb5e603c25d38283
Administrator <Administrator@DESKTOP-SFUFKH6>
2019-05-24 20:58:34 +0800

    commit by xiongwenyan

--- src/web/bookStore/test/Test_user.java
+++ src/web/bookStore/test/Test_user.java
@@ -1,6 +1,5 @@
 package web.bookStore.test;
 
-import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,7 +8,7 @@ import web.bookStore.service.UserService;
 
 public class Test_user {
 
-	@Test
+	
 	public void login() {
 		ApplicationContext applicationContext = new   ClassPathXmlApplicationContext("application-dao.xml","application-service.xml");
 		 IUserService iUserService = applicationContext.getBean("userService", UserService.class);
